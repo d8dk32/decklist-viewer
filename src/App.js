a94270374c1d2dd8add84a93ceac55f6146283fa
Lee <lee@system76-pc.localdomain>
2020-02-02 21:51:45 -0500

    basic functionality in place

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Typography, Grid, Paper } from '@material-ui/core';
 
 function App() {
+
+  const deck = require('./decklists/EchoDD.json');
+  const maincards = Object.keys(deck.main);
+  const sidecards = Object.keys(deck.side);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container>
+        {
+          maincards.map((it, index) => {
+            return (<Grid item xs>
+                      <Paper style={{margin: "2px", backgroundColor:"LightSteelBlue", paddingRight:"5px", paddingLeft: "5px"}}>
+                        <div>
+                          <Typography noWrap key={index}>{deck.main[it]} {it}</Typography>
+                        </div>
+                      </Paper>
+                    </Grid>)
+        })
+        }
+       </Grid>
+       <hr/>
+       <Grid container>
+        {
+          sidecards.map((it, index) => {
+            return (<Grid item xs>
+                      <Paper style={{margin: "2px", backgroundColor:"Gainsboro", paddingRight:"5px", paddingLeft: "5px"}}>
+                        <div>
+                          <Typography style={{margin: "2px"}} noWrap key={index}> {deck.side[it]} {it}</Typography>
+                        </div>
+                      </Paper>
+                    </Grid>)
+        })
+        }
+      </Grid>
     </div>
   );
 }
